aa682960892c6422bf771b43e629c496362f3d98
王漩波 <1345545983@qq.com>
2020-09-01 20:37:31 +0800

    :tada:

---
+++ chaincode/src/go/test/test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
+)
+
+// Asset chaincode implementation
+type Asset struct {
+}
+
+// Init initializes chaincode
+func (t *Asset) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	return shim.Success(nil)
+}
+
+// Invoke entry point for Invocations
+func (t *Asset) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	fn, args := stub.GetFunctionAndParameters()
+	fmt.Println("Invoke function: " + fn)
+
+	switch fn {
+	case "set":
+		return set(stub, args)
+	case "get":
+		return get(stub, args)
+	default:
+		return shim.Error("Received unknown function invocation")
+	}
+}
+
+func set(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 2 {
+		return shim.Error("Incorrect arguments, expecting a key and a value")
+	}
+
+	if err := stub.PutState(args[0], []byte(args[1])); err != nil {
+		return shim.Error("Failed to set asset")
+	}
+
+	txID := []byte(stub.GetTxID())
+
+	return shim.Success(txID)
+}
+
+func get(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 1 {
+		return shim.Error("Incorrect arguments, expecting a key")
+	}
+
+	value, err := stub.GetState(args[0])
+	if err != nil {
+		return shim.Error("Failed to get asset")
+	}
+
+	return shim.Success(value)
+}
+
+func main() {
+	err := shim.Start(new(Asset))
+	if err != nil {
+		fmt.Printf("Error starting Asset chaincode: %s", err)
+	}
+}
