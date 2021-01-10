80fff5de60ef241e4a6debe9e0cb2e9fff18f608
王漩波 <1345545983@qq.com>
2021-01-10 14:15:47 +0800

    :bug: Fix chaincode set method

--- chaincode/src/go/test/test.go
+++ chaincode/src/go/test/test.go
@@ -32,7 +32,7 @@ func (t *Asset) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 }
 
 func set(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 3 {
+	if len(args) != 2 {
 		return shim.Error("Incorrect arguments, expecting a key and a value")
 	}
 
